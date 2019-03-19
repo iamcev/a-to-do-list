63720e1936e2e17a53eb8cd3602df1ee7950ff12
Dmitry Kornilov <47787102+DimaTheGoat@users.noreply.github.com>
2019-03-19 15:12:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ document.querySelector('#themes-btn').addEventListener('click',()=>{document.que
 document.querySelector('.back').addEventListener('click',()=>{document.querySelector('#themes').classList.remove('open');});
 document.querySelector('#light').addEventListener('click',()=>{document.body.style.background="white";document.body.style.color="black";})
 document.querySelector('#dark').addEventListener('click',()=>{document.body.style.background="#333";document.body.style.color="white";})
+document.querySelector('#red').addEventListener('click',()=>{document.body.style.background="red";document.body.style.color="blue";})
